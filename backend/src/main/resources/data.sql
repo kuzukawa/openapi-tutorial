61ac28724339de2f299b3375064fec38fda72393
Keisuke Kuzukawa <kuzukawa@gmail.com>
2024-02-29 22:50:36 +0900

    feat: add H2 Database

---
+++ backend/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO ARTISTS VALUES ('kuzukawa', 'kuzukawas','rock',2);
+INSERT INTO ARTISTS VALUES ('Eminem', 'Eminem','hiphop',11);
+INSERT INTO ARTISTS VALUES ('mozart', 'mozart','classic',99);
